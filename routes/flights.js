c1ab4209d18fe9f64bb366f02e1350af447e349f
smkelly1691 <skelly1691@gmail.com>
2022-09-30 15:13:00 -0700

    adds initial folders and configuration

---
+++ routes/flights.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const flightsCtrl = require('../controllers/flights');
+
+// All the routes already start with '/flights'
+
+
+
+module.exports = router;
