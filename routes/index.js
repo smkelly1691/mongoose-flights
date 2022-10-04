01c020b1dc1fad9342922f93dd4c7ecc6ff6cb80
smkelly1691 <skelly1691@gmail.com>
2022-10-04 15:01:35 -0700

    removes route to home page and sets up redirect to index view

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,9 @@ var express = require('express');
 var router = express.Router();
 const indexCtrlr = require('../controllers/index');
 
-/* GET home page. */
-router.get('/', indexCtrlr.index);
+/* Go directly to Index view. */
+router.get('/', function(req, res, next) {
+  res.redirect('/flights');
+});
 
 module.exports = router;
